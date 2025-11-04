132b2c353308cb1d2a620cdc10642aed42516145
Juanmg <gutierrezfierrojuanmanuel@gmail.com>
2025-11-04 09:08:42 -0500

    hotfix-dev: new atributte in person

--- Entity/Models/SecurityModule/Person.cs
+++ Entity/Models/SecurityModule/Person.cs
@@ -10,6 +10,7 @@ namespace Entity.Models.SecurityModule
         public string DocumentType { get; set; } = string.Empty;
         public string DocumentNumber { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
 
         // Propiedad de Navegacion Inversa
         public User User { get; set; } = null!;
